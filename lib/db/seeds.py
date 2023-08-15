b4d7fd8c91ab2ef9c9f87b124e6ee20463f99ed0
Aleksandr <titamoto@gmail.com>
2023-08-15 13:57:08 -0700

    get rid of the test table

--- lib/db/seeds.py
+++ lib/db/seeds.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 from session import session
-from faker import Faker
 from test import questions
 from idols import populate_idols
+# from faker import Faker
 # import random
 
-from models import Idol, User, Test
+from models import Idol, User
 
-fake = Faker()
-
-if __name__ == '__main__':
-    pass
+# fake = Faker()
 
 def delete_records():
     session.query(Idol).delete()
     session.query(User).delete()
-    session.query(Test).delete()
     session.commit()
 
 def create_records():
     populate_idols()
+    # test_user = User(email='user@test.com')
 
+if __name__ == '__main__':
+    delete_records()
+    create_records()
 
 
 
---
+++ lib/debug.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from db.models import Idol, User
+from db.session import session
+
+if __name__ == '__main__':
+    
+    import ipdb; ipdb.set_trace()
