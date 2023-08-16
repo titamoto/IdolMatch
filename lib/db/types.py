5ff5711b0b0fe49370bfe5168d4fcc6062c93e97
Aleksandr <titamoto@gmail.com>
2023-08-16 11:42:03 -0700

    make types file

---
+++ lib/db/types.py
@@ -0,0 +1,18 @@
+types = {
+    'ISTJ' : 'The Inspector',
+    'ISFJ' : 'The Protector',
+    'INFJ' : 'The Counselor',
+    'INTJ' : 'The Mastermind',
+    'ISTP' : 'The Craftsman',
+    'ISFP' : 'The Composer',
+    'INFP' : 'The Healer',
+    'INTP' : 'The Architect',
+    'ESTP' : 'The Dynamo',
+    'ESFP' : 'The Performer',
+    'ENFP' : 'The Champion',
+    'ENTP' : 'The Visionary',
+    'ESTJ' : 'The Supervisor',
+    'ESFJ' : 'The Provider',
+    'ENFJ' : 'The Teacher',
+    'ENTJ' : 'The Commander'
+}
