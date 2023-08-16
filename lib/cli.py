f5d3fd1a35154258c2a7f9211cce1bd081ca8a48
Aleksandr <titamoto@gmail.com>
2023-08-16 11:07:26 -0700

    create calculate_result func

--- lib/cli.py
+++ lib/cli.py
@@ -48,19 +48,35 @@ if __name__ == '__main__':
         answers = []
         count = 0
         while count < len(questions):
-            ask_question()
+            answer = ask_question(count)
             count += 1
             answers.append(answer)
-        result = calculate_result()
-        persist_result(email, result)
+        result = calculate_result(answers)
+        #create a user record in the database:
+        User.persist_result(email, result)
         show_result(email)
     
-    def ask_question():
-        pass
+    def ask_question(count):
+        print (
+            f'{questions[count]}'\
+            'Type a number from 1 to 5, where "1" corresponds with the left statement, "5"--with the right, and "3" is neutral:'
+                )
+        answer = input()
+        if answer not in range(1, 6):
+            print(f'{answer} is not a number from 1 to 5. Try again:')
+        return answer
 
-    def calculate_result():
-        pass
-    
+    def calculate_result(a):
+        ie = 30-a[2]-a[6]-a[10]+a[14]-a[18]+a[22]+a[26]-a[30]
+        sn = 12+a[3]+a[7]+a[11]+a[15]+a[19]-a[23]-a[27]+a[31]
+        ft = 30-a[1]+a[5]+a[9]-a[13]-a[17]+a[21]-a[25]-a[29]
+        jp = 18+a[0]+a[4]-a[8]+a[12]-a[16]+a[20]-a[24]+a[28]
+        result = ''
+        result[0] = 'E' if ie > 24 else 'I'
+        result[1] = 'N' if sn > 24 else 'S'
+        result[2] = 'T' if ft > 24 else 'F'
+        result[3] = 'P' if jp > 24 else 'J'
+        return result 
     #run main menu:
     main()
     
