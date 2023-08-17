a4457a05fd31480658be11d6fe50224f1bb27130
Aleksandr <titamoto@gmail.com>
2023-08-17 06:31:18 -0700

    clean idols.py

--- lib/db/idols.py
+++ lib/db/idols.py
@@ -4,56 +4,57 @@ from db.models import Idol
 from db.session import session
 
 def populate_idols():
-    # Jin: INTP, The Thinker, match is ENTJ, ENFJ; 
     idols = []
+
     jin = Idol(
         name='Jin',
         type='INTP',
-        type_alias='The Thinker',
+        type_alias='The Architect',
         match_type='ENTJ, ENFJ'
     )
-   # Jungkook: INTP, The Thinker, match is ENTJ, ENFJ
+
     jungkook = Idol(
         name='Jungkook',
         type='INTP',
-        type_alias='The Thinker',
+        type_alias='The Architect',
         match_type='ENTJ, ENFJ'
     )
-    # Suga: ISTP, The Crafter, match is ESTJ, ESFJ;
+
     suga = Idol(
         name='Suga',
         type='ISTP',
-        type_alias='The Crafter',
+        type_alias='The Craftsman',
         match_type='ESTJ, ESFJ'
     )
-    # J-Hope: INFJ, The Advocate, match is ENFP, ENTP;
+  
     jhope = Idol(
         name='J-Hope',
         type='INFJ',
-        type_alias='The Advocate',
+        type_alias='The Counselor',
         match_type='ENFP, ENTP'
     )
-    # RM: ENFP, The Champion, match is INTJ, INFJ;
+ 
     rm = Idol(
         name='RM',
         type='ENFP',
         type_alias='The Champion',
         match_type='INTJ, INFJ'
     )
-   # Jimin: ESTP, The Persuader, match is ISFJ, ISTJ;
+
     jimin = Idol(
         name='Jimin',
         type='ESTP',
-        type_alias='The Persuader',
+        type_alias='The Dynamo',
         match_type='ISFJ, ISTJ'
     )
-    # V: INFP, The Mediator, ENTJ, ENFJ;
+
     v = Idol(
         name='V',
         type='INFP',
-        type_alias='The Mediator',
+        type_alias='The Healer',
         match_type='ENTJ, ENFJ'
     )
+    
     idols = [jin, jungkook, v, jimin, rm, jhope, suga]
     session.add_all(idols)
     session.commit()
