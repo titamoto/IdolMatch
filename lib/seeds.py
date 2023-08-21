695ef249c96f39c206da28c1281d6aaa2704bde0
Aleksandr <titamoto@gmail.com>
2023-08-20 19:15:54 -0700

    add seeded users

--- lib/seeds.py
+++ lib/seeds.py
@@ -2,12 +2,12 @@
 from session import session
 from data.idols import populate_idols
 from data.types import types, find_type_dict
-# from faker import Faker
-# import random
+from faker import Faker
+import random
 
 from data.models import Idol, User
 
-# fake = Faker()
+fake = Faker()
 
 def seed():
 
@@ -16,13 +16,20 @@ def seed():
         session.query(User).delete()
         session.commit()
 
-    def popelate_users():
-        users = [User() for i in range(20)]
 
     def create_records():
         populate_idols()
         test_user = User(email='user@test.com', type='ENTJ', type_alias=find_type_dict('ENTJ')['alias'])
-        users = [User() for i in range(20)]
+        users = []
+        for i in range(20):
+            user = User(
+                email=fake.email(),
+                type=random.choice([type['type'] for type in types])
+            )
+            user.type_alias=find_type_dict(user.type)['alias']
+            session.add(user)
+            session.commit()
+            users.append(user)
 
         session.add(test_user)
         session.commit()
